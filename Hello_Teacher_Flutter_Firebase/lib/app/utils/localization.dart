6b0208366031711b3676f4c55020def9ef86239a
Ebtehalla <abthala935@gmail,com>
2024-02-05 00:42:47 +0300

    new update

--- Hello_Teacher_Flutter_Firebase/lib/app/utils/localization.dart
+++ Hello_Teacher_Flutter_Firebase/lib/app/utils/localization.dart
@@ -3,21 +3,25 @@ import 'dart:ui';
 import 'package:get/get.dart';
 import 'package:halo_teacher/app/translation/fr.dart';
 
-import '../translation/en_US.dart';
+import '../translation/sa_SA.dart';
 
 class LocalizationService extends Translations {
-  static final locale = Locale('en', "US");
+  static final locale = Locale('sa', "SA");
 
-  static final langs = ['English', "Urdu", 'France'];
+  static final langs = ['English', "Urdu", 'France', 'Saudi Arabia'];
 
   static final locales = [
     Locale('en', 'US'),
     Locale('ur', 'PK'),
+    Locale('sa', 'SA'),
     Locale('fr', null)
   ];
   @override
   // TODO: implement keys
-  Map<String, Map<String, String>> get keys => {'en_US': en, 'fr': fr};
+  Map<String, Map<String, String>> get keys => {
+        'sa_SA': sa,
+        'fr': fr,
+      };
 
   void changeLocale(String lang) {
     final locale = _getLocaleFromLanguage(lang);
